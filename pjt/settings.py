0cee3ae7544874e2a6fb0ca4337f28edc0f2ed78
forwardyoung <chahwayoung214@gmail.com>
2022-11-01 11:32:52 +0900

    app_name 수정

--- pjt/settings.py
+++ pjt/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
-    'accounts',
+    'reviews',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
