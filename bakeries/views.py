38031b0b2c9fd966d508cc0da95c86135b2cfc27
hyejinny97 <yhj970102@gmail.com>
2022-11-05 19:45:44 +0900

    Create-search

--- bakeries/views.py
+++ bakeries/views.py
@@ -234,3 +234,66 @@ def shop_home(request, shop_id):
   }
 
   return render(request, 'bakeries/shop_home.html', context)
+
+# 빵집 검색(search) 기능
+def search(request):
+  query = request.GET.get('query')
+
+  base_url = 'https://www.siksinhot.com/search?keywords=' # 식신페이지
+  keyword = f'{query} 빵집' # 검색어
+  search_url = base_url + keyword # 식신페이지 + 검색어
+  r = requests.get(search_url) # 페이지에 요청하기
+  soup = BeautifulSoup(r.text, "html.parser") # 요청한 데이터 텍스트로 불러오기
+
+  store_DB = [] # 한 페이지 내 모든 가게의 데이터
+  '''
+  [
+    {
+      'store_name': 식당이름,
+      'store_score': 평점,
+      'store_img': 썸네일,
+      'store_region': 위치정보,
+      'store_category': 가게 업종,   
+      'store_id': 가게 url,
+      'review_cnt': 식당에 달린 리뷰 개수,
+    }
+  ]
+  '''
+
+  store_name = soup.select(".textBox > h2") # 식당 이름
+  store_score = soup.select(".textBox > span") # 식당 평점
+  store_img = soup.select("figure > a > img") # 식당 이미지 주소
+  store_region = soup.select('.cate > a') # 식당 간단정보
+  store_id = soup.select('figure > a') # 식당 url
+
+  # 식당 이미지 주소 정제
+  store_img_rst = []
+  for store in store_img:
+      if store['alt'] != 'best':
+          store_img_rst.append(store)
+
+  # 가게의 id 알아내기 위한 정규 표현식
+  # 예) https://www.siksinhot.com/P/329263 에서 '329263'만 뽑아냄
+  p = re.compile('[0-9]+')
+
+  for i in range(0, len(store_name)):
+      store_data = {}
+      if store_region[2 * i + 1].text in ['베이커리/제과점', '카페/커피숍', '샌드위치', '브런치']:
+        store_data['store_name'] = store_name[i].text
+        store_data['store_score'] = store_score[i].text
+        store_data['store_img'] = store_img_rst[i]['src']
+        store_data['store_region'] = store_region[i * 2].text
+        store_data['store_category'] = store_region[2 * i + 1].text
+        
+        store_id_rst = p.search(store_id[i]['href']).group()
+        store_data['store_id'] = store_id_rst
+        store_data['review_cnt'] = Review.objects.filter(shop_id=int(store_id_rst)).count
+
+        store_DB.append(store_data)
+
+  context = {
+      'store_DB': store_DB,
+      'query': query,
+  }
+
+  return render(request, 'bakeries/shop_list.html', context)
